107f4a12ad3c421d99edb729d25e9b24539c478d
cheygaby16 <cheyegaby@gmail.com>
2021-07-18 14:47:42 -0500

    Imágenes de portada página promociones (corregir)

--- app/Views/vpromociones.php
+++ app/Views/vpromociones.php
@@ -1,18 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Promociones</title>
-    <link rel="stylesheet" href="main.css">
-</head>
-<body>
-<div id="games">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Promociones</title>
+            <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+    <!-- Carousel -->
+        <div id="mainSlider" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/Promo1.png" class="d-block w-100" alt="" height="750">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Promo2.png" class="d-block w-100" alt="" height="750">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Promo3.png" class="d-block w-100" alt="" height="750">
+                </div>
+            </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#mainSlider" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#mainSlider" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+        </div>
+
+        <div id="games">
         <div class="container-md p-5">
             <div class="row pt-5">
 
                 <h3 class="text-center pb-5 pt-5 h1"><br> Promociones </h3>
                 
             </div>
-</body>
+    </body>
 </html>
